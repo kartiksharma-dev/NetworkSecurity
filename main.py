07cc9984b7a4b35c035dedf41e6519928ca923fd
kar-tik11 <snk32706@gmail.com>
2026-06-18 01:52:34 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 from networksecurity.components.data_validation import DataValidation
@@ -26,6 +27,14 @@ if __name__=='__main__':
        data_validation_artifact=data_validation.initiate_data_validation()
        logging.info("Data valiadtion completed")
        print(data_validation_artifact)
+
+
+       data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+       logging.info("data Transforamtion started")
+       data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+       data_transformation_artifact=data_transformation.initiate_data_transformation()
+       print(data_transformation_artifact)
+       logging.info("data Transforamtion started")
 
        
        
