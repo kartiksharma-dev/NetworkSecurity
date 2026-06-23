90a23cfd2080e17a998a050980f2252803b9acec
kar-tik11 <snk32706@gmail.com>
2026-06-23 09:41:39 +0530

    cloud s data storage

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -11,8 +11,10 @@ class TrainingPipelineConfig():
         timestamp=timestamp.strftime("%m_%d%Y_%H_%M_%S")
         self.pipeline_name=training_pipeline.PIPELINE_NAME
         self.artifact_name=training_pipeline.ARTIFACT_DIR
-        self.artifact_dir=os.path.join(self.artifact_name,timestamp)
-        self.timestamp: str=timestamp   
+        self.artifact_dir=os.path.join(self.artifact_name,timestamp) 
+        self.model_dir=os.path.join("final_model")
+        self.timestamp: str=timestamp 
+ 
 
 
 
